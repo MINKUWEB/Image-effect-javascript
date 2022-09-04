5b86df9daf836dc82590ef93e7db5e0d495f8349
Abhishk Anand <43206164+MINKUWEB@users.noreply.github.com>
2022-09-05 00:52:22 +0530

    Add files via upload

---
+++ new.js
@@ -0,0 +1,20 @@
+
+		function brightnessess(){
+	var x=document.getElementById('bright').value;
+	var y=document.getElementById('pics');
+	y.style.filter="brightness("+x+"%)";
+}
+
+		function contrassness(){
+	var x=document.getElementById('contrass').value;
+	var y=document.getElementById('pics');
+	y.style.filter="brightness("+x+"%)";
+}
+
+		function saturatesness(){
+	var x=document.getElementById('saturates').value;
+	var y=document.getElementById('pics');
+	y.style.filter="saturate("+x+"%)";
+}
+
+	
